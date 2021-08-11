9b76d13228459516065951a20f856f310dfb6302
juliwind <julianwindheuser@gmail.com>
2021-08-12 00:24:25 +0200

    Update quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,40 +1,4 @@
-/*function quicksortChange(array, left, right) {
-    let pivot_number = Math.floor(array.length / 2);
-    let pivot = array[pivot_number];
 
-    while (left <= right) {
-        while (array[left] < pivot) {
-            left++;
-        }
-        while (array[right] > pivot) {
-            right--;
-        }
-        if (left <= right) {
-            // SWITCH
-            a = array[left];
-            b = array[right];
-            array[left] = b;
-            array[right] = a;
-            left++;
-            right--;
-        }
-    }
-    return left;
-}
-
-function quicksort(array, left, right) {
-    if (array.length > 1) {
-        let sort = quicksortChange(array, left, right);
-        if (left < sort - 1) {
-            quicksort(array, left, sort - 1)
-        }
-        if (right > sort) {
-            quicksort(array, sort, right);
-        }
-    }
-    return array;
-}
-*/
 function quicksortChange(array, left, right) {
     let pivot   = array[Math.floor((right + left) / 2)]
     while (left <= right) {
@@ -58,11 +22,11 @@ function quicksortChange(array, left, right) {
 
 function quickSort(array, left, right) {
     if (array.length > 1) {
-        let operator = quicksortChange(array, left, right); //index returned from partition
-        if (left < operator - 1) { //more elements on the left side of the pivot
+        let operator = quicksortChange(array, left, right);
+        if (left < operator - 1) {
             quickSort(array, left, operator - 1);
         }
-        if (right > operator) { //more elements on the right side of the pivot
+        if (right > operator) {
             quickSort(array, operator, right);
         }
     }
